273f4ba4bbd312afa496cf26392532dc11ec755c
xewoiy <86353201+xewoiy@users.noreply.github.com>
2025-10-23 18:50:10 +0700

    Update LaunchController.cpp
    Signed-off-by: xewoiy <86353201+xewoiy@users.noreply.github.com>

--- launcher/LaunchController.cpp
+++ launcher/LaunchController.cpp
@@ -85,27 +85,22 @@ void LaunchController::decideAccount()
         return;
     }
 
-    // Find an account to use.
     auto accounts = APPLICATION->accounts();
-    /*if (accounts->count() <= 0 || !accounts->anyAccountIsValid()) {
-        // Tell the user they need to log in at least one account in order to play.
-        auto reply = CustomMessageBox::selectable(m_parentWidget, tr("No Accounts"),
-                                                  tr("In order to play Minecraft, you must have at least one Microsoft "
-                                                     "account which owns Minecraft logged in. "
-                                                     "Would you like to open the account manager to add an account now?"),
-                                                  QMessageBox::Information, QMessageBox::Yes | QMessageBox::No)
-                         ->exec();
 
-        if (reply == QMessageBox::Yes) {
-            // Open the account manager.
-            APPLICATION->ShowGlobalSettings(m_parentWidget, "accounts");
-        } else if (reply == QMessageBox::No) {
-            // Do not open "profile select" dialog.
-            return;
-        }
-    }*/
+    if (accounts->count() == 0) {
+        auto offlineAccount = std::make_shared<MinecraftAccount>();
+        offlineAccount->setAccountType(AccountType::Offline);
+        offlineAccount->setUsername("Player");
+        offlineAccount->setProfileName("Player");
+        offlineAccount->setUUID(MinecraftAccount::uuidFromUsername("Player"));
+        offlineAccount->setAccessToken("offline");
+        offlineAccount->setSessionId("offline");
+        offlineAccount->setAccountState(AccountState::Online);
+        accounts->addAccount(offlineAccount);
+        accounts->setDefaultAccount(offlineAccount);
+    }
 
-    // Select the account to use. If the instance has a specific account set, that will be used. Otherwise, the default account will be used
+
     auto instanceAccountId = m_instance->settings()->get("InstanceAccountId").toString();
     auto instanceAccountIndex = accounts->findAccountByProfileId(instanceAccountId);
     if (instanceAccountIndex == -1 || instanceAccountId.isEmpty()) {
@@ -114,20 +109,9 @@ void LaunchController::decideAccount()
         m_accountToUse = accounts->at(instanceAccountIndex);
     }
 
+
     if (!m_accountToUse) {
-        // If no default account is set, ask the user which one to use.
-        ProfileSelectDialog selectDialog(tr("Which account would you like to use?"), ProfileSelectDialog::GlobalDefaultCheckbox,
-                                         m_parentWidget);
-
-        selectDialog.exec();
-
-        // Launch the instance with the selected account.
-        m_accountToUse = selectDialog.selectedAccount();
-
-        // If the user said to use the account as default, do that.
-        if (selectDialog.useAsGlobalDefault() && m_accountToUse) {
-            accounts->setDefaultAccount(m_accountToUse);
-        }
+        m_accountToUse = accounts->defaultAccount();
     }
 }
 
